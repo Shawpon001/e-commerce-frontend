d6798ae12fbb09415b682b0a4525a4c61b6e4765
hridayroy19 <hrhridoyroy503@gmail.com>
2025-01-27 11:28:16 +0600

    cret a book detail page

---
+++ src/router/router.tsx
@@ -0,0 +1,67 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayOut from "../mainLayOut/MainLayOut";
+import Home from "../page/Home/Home";
+import Login from "../page/authprovider/Login";
+import Registation from "../page/authprovider/Registation";
+import DashboardLayout from "../mainLayOut/Deshboar";
+import About from "../page/about/About";
+import Bloge from "../page/bloge/Bloge";
+import Contact from "../page/contact/Contact";
+import Book from "../page/book/Book";
+import BookDetails from "../page/book/BookDetails";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayOut />,
+    //   errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/books",
+        element: <Book />,
+        children:[
+          {
+            path:"books-main",
+            element:<Bloge/>
+          },
+          
+        ]
+      },
+      {
+        path:"/books/detais",
+        element:<BookDetails/>
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/bloge",
+        element: <Bloge />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      // login routes
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/registaion",
+        element: <Registation />,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+  },
+]);
+
+export default router;
